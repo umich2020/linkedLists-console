bb61965c3bc5d807e830afe887e3c9ada1a63f6c
umich2020 <jimson@umich.com>
2025-03-25 01:32:00 -0400

    finished with basica functions

--- factory.js
+++ factory.js
@@ -15,7 +15,39 @@ function LinkedList () {
             list.unshift(node(value,list.at(0).value))
         }
     }
-    return {list, append,prepend}
+    function size() {
+        return list.length
+    }
+    function head() {
+        return JSON.stringify(list.at(0))
+    }
+    function tail() {
+        return JSON.stringify(list.at(-1))
+    }
+    function at(index) {
+        return JSON.stringify(list.at(index))
+    }
+    function pop () {
+        list.pop()
+    }
+    function find(search) {
+        for (let i =0; i<list.length;i++) {
+            if(list.at(i).value === search) {
+                return true
+            }
+        }
+        return false
+
+    }
+    function toString() {
+        let str = ""
+        for (let i=0; i<list.length;i++) {
+            str += "( "+list.at(i).value+" ) -> "
+        }
+        str+= "null"
+        return str
+    }
+    return {list, append,prepend,size,head,tail,at,pop,find,toString}
 
 }
 function node (value=null, nextNode = null) {
@@ -25,7 +57,12 @@ const list = new LinkedList();
 
 list.append("dog");
 list.append("cat");
-list.prepend("parrot");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
 console.log(list.list)
+console.log(list.toString())
+
 
